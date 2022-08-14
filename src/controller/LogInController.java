e0dc8da45603a4ed9cb380f81b87faf5533fd482
rashidya <rashmiliyanage2000@gmail.com>
2022-08-15 01:39:13 +0530

    Repo Created

---
+++ src/controller/LogInController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LogInController {
+    public AnchorPane logInFormContext;
+    public TextField txtUserName;
+
+    public void logInOnAction(ActionEvent actionEvent) {
+    }
+}
