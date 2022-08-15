e71017bf481ae5508e3e50c54d6cfb77f5dec188
rashidya <rashmiliyanage2000@gmail.com>
2022-08-16 05:17:54 +0530

    Updated

--- src/controller/ChatFormController.java
+++ src/controller/ChatFormController.java
@@ -3,7 +3,10 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import model.ServerSide;
 
 import javax.imageio.ImageIO;
@@ -16,6 +19,7 @@ public class ChatFormController {
     public AnchorPane chatFormContext;
     public TextArea txtArea;
     public TextField txtMessage;
+    public AnchorPane imageContext;
     private Socket socket;
     private BufferedReader bufferedReader;
     private BufferedWriter bufferedWriter;
@@ -40,6 +44,9 @@ public class ChatFormController {
                     try{
                         chatMessages = bufferedReader.readLine();
                         txtArea.appendText(chatMessages+"\n");
+
+
+
 
                     }catch (IOException e){
                         close(socket,bufferedWriter,bufferedReader);
@@ -86,7 +93,30 @@ public class ChatFormController {
     }
 
     public void sendPhotoOnAction(ActionEvent actionEvent) throws IOException {
+       // Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose a Image");
+        File file = fileChooser.showOpenDialog(null);
 
+        bufferedWriter.write(userName + " : " + file.getPath());
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+
+        Image image = new Image("src/view/img/chat.png");
+        //Creating the image view
+        ImageView imageView = new ImageView();
+        //Setting image to the image view
+        imageView.setImage(image);
+        //Setting the image view parameters
+        imageView.setX(5);
+        imageView.setY(0);
+        imageView.setFitWidth(55);
+        imageView.setPreserveRatio(true);
+        imageContext.getChildren().addAll(imageView);
+
+
+
+        //txtClientMessage.setText(filePath.getPath());
     }
 
 
